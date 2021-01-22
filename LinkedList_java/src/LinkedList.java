93fbd349ef788ce0ae6f7048649086c7b089ee78
ParkBeomSu <qjatn8595@gmail.com>
2021-01-22 21:20:23 +0900

    노드 수정 기능 추가

--- LinkedList_java/src/LinkedList.java
+++ LinkedList_java/src/LinkedList.java
@@ -43,12 +43,14 @@ public class LinkedList {
         // 첫번째 원소를 지울 경우
         if(head.data == input){
             head = head.next;
+            size--;
             return;
         }
 
         while(tmp.next.data != input){
             tmp = tmp.next;
             if(tmp.next.data == input){
+                size--;
                 System.out.printf("=== 찾았음 %d 얘 다음꺼 지울거야 !\n\n", tmp.data);
                 if(tmp.next == tail){
                     tail = tmp;
@@ -81,8 +83,16 @@ public class LinkedList {
         Node n = new Node(input);
         tmp1.next = n;      // 그 노드의 next 를 새로 추가할 노드와 연결하고
         n.next = tmp2;
+        size++;
     }
 
+    public void Modify(int k, int input){
+        if(k==0){
+            System.out.println("수정할 노드가 없습니다.");
+        }
+        Node tmp1 = node(k);
+        tmp1.data = input;
+    }
 
     public void PrintList(){
         tmp = head;
--- LinkedList_java/src/Main.java
+++ LinkedList_java/src/Main.java
@@ -4,14 +4,12 @@ public class Main {
         L1.addLast(10);
         L1.addLast(20);
         L1.addLast(30);
-        L1.PrintList();
-        System.out.println("");
-        L1.addFirst(40);
+        L1.addLast(40);
         L1.addLast(50);
-        L1.delete(40);
-        L1.addLast(60);
-        L1.addLast(70);
-        L1.add(2,22);
+        L1.addFirst(1);
+        L1.add(2,33);
+        L1.delete(20);
+        L1.Modify(1,39);
         L1.PrintList();
     }
 }
